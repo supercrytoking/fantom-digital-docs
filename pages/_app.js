7cfa9faf911df448ac2e7ac7ac782744381cdda1
supercrytoking <108713886+supercrytoking@users.noreply.github.com>
2021-05-25 01:49:35 -0400

    Updated Docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import { GeistProvider, CssBaseline } from '@geist-ui/react'
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GeistProvider>
+      <Component {...pageProps} />
+    </GeistProvider>
+  )
 }
